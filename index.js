be25ae18dc504df8d137079b1b73ab07eaa9ceda
Brian Woodward <brian.woodward@gmail.com>
2016-02-01 16:25:56 -0500

    init

---
+++ index.js
@@ -0,0 +1,12 @@
+/*!
+ * base-compose <https://github.com/base/base-compose>
+ *
+ * Copyright (c) 2016, Brian Woodward.
+ * Licensed under the MIT License.
+ */
+
+'use strict';
+
+module.exports = function () {
+  // do stuff
+};
